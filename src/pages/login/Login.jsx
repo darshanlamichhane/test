ab488d4646bf644f02a9c27c090e52fe48328fb0
unknown <darshanlamichhane012@gmail.com>
2023-04-25 08:32:47 +0545

    changes to reacr-app

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,34 @@
 import { Link } from 'react-router-dom';
 import './login.scss';
 
+
 const Login=()=>{
     return(
     <div className='login'>
-        <h1>This is Login</h1>
-        <button>Login</button>
-        <Link to ='/register'>
-        <button>Register instead</button>
-        </Link>
+        <div className="card">
+            <div className="left">
+                <h1>Find Your Companion</h1>
+                <p>Find people having similar
+                    interests as yourself and build connections.
+                    Explore the feeling of being able to talk 
+                    to someone who shares the same interests as you.  
+                </p>
+                <span>Don't have an account?</span>
+                <Link to ='/register'>
+                <button>Register</button>
+                </Link>  
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type='text' placeholder='Username/Email Id'/>
+                    <input type='password' placeholder='Password'/>
+                    <button>Login</button>
+                </form>
+            </div>
         </div>
+    
+    </div>
     );
 }
 export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,11 +5,31 @@ import './register.scss'
 function Register() {
   return (
     <div className='register'>
-      <h1>This is register</h1>
+      <div className='card'>
+      <div className='left'>
+        <h1>Register</h1>
+        <form>
+          <input type='text'placeholder='Username' />
+          <input type='text' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <input type='text' placeholder='Full-Name' />
+        </form>
       <button>Register</button>
+      </div>
+      <div className='right'>
+      <h1>Find Your Companion</h1>
+      <p>Find people having similar
+                    interests as yourself and build connections.
+                    Explore the feeling of being able to talk 
+                    to someone who shares the same interests as you.  
+      </p>
+      <span>Already have an account?</span>
+      
       <Link to ='/login'>
-      <button>Login Instead</button>
+      <button style={{cursor:'pointer'}}>Login</button>
       </Link>
+      </div>
+      </div>
     </div>
   )
 }
